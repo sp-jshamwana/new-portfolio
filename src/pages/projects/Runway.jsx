7202df28175e5390c9aba417b7cfb29b73988f0f
sp-jonathan <jonathan.shamwana@siphox.com>
2024-08-06 09:57:55 -0400

    create page for iRunway Project

--- src/pages/projects/Runway.jsx
+++ src/pages/projects/Runway.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
+import Navbar from '../../components/Navbar';
 
+const RunwayProject = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
+
+export default RunwayProject
