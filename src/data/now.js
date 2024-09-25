16c073ca0663433128d0ef7ef65eeddc3253a929
sp-jonathan <jonathan.shamwana@siphox.com>
2024-09-25 16:11:52 -0300

    update the now page

--- src/data/now.js
+++ src/data/now.js
@@ -2,8 +2,10 @@ const items = [
     { text: 'Searching for a Product Management internship for Summer 2025', tag: 'career' },
     { text: 'Hosting the Humans of Minerva podcast', tag: 'school' },
     { text: 'Building GenZed, a community of young Computer Science students in my home country, Zambia.', tag: 'career' },
-    { text: 'Training to run a 2:55 marathon on August 18th', tag: 'running' },
+    { text: 'Training to run a 2:49 marathon on December 1st', tag: 'running' },
     { text: "Sending out a private weekly newsletter.", tag: 'art' },
+    { text: 'Learning more about healthcare and sustainable fashion for my senior-year capstone project.', tag: 'career' },
+    { text: "Interning at Nutrix AI, Argentina's first AI nutritional assistant.", tag: 'career' },
     { text: "Coursework: CS162: Building Powerful Software Applications; CS114: Probability & Statistics, AH146: Decoding Arts and Literature", tag: 'school'},
   ];
 
