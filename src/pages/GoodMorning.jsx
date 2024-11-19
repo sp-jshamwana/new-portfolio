230404ca548b425d0af764319750fa9d84c30d3a
sp-jonathan <jonathanshamwana.visa@gmail.com>
2024-11-19 07:43:10 -0300

    create temp good morning page

---
+++ src/pages/GoodMorning.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../index.css';
+
+const GoodMorning = () => {
+
+    return (
+        <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+            <h1 className="text-7xl text-purple-900">good morning mushroom</h1>
+        </div>
+    );
+}
+
+export default GoodMorning;
